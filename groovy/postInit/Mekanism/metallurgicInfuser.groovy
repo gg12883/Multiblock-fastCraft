12487ed2c649b0e670c633d3b91c3791e030c12b
gg12883 <186947722+gg12883@users.noreply.github.com>
2025-01-08 03:37:50 +0900

    濃縮合金のレシピを本家に合わせる config更新

---
+++ groovy/postInit/Mekanism/metallurgicInfuser.groovy
@@ -0,0 +1,8 @@
+
+mods.mekanism.metallurgicInfuser.recipeBuilder()
+   .input(item("minecraft:iron_ingot"))
+   .amount(10)
+   .infuse(infusionType("REDSTONE"))
+   .output(item('mekanism:enrichedalloy'))
+   .register()
+   
